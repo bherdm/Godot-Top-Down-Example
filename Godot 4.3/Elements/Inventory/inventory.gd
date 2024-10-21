11c23084291bbc9282e53a50cca32cd435f17810
bherdm <146450154+bherdm@users.noreply.github.com>
2024-10-21 06:43:09 -0500

    Inventory has specified size
    Inventory fills itself with empty slots on initialization and pickups will not react if inventory is full. Previously this script would append to the _content array when new items were collided with, now, after looking for partially full slots, it looks for slots with the 'empty' item to replace.
    
    Known issue: if pickups can be partially picked up, the pickup on the ground is not reduced by the amount picked up by the player, creating a duplication of those items.

--- Godot 4.3/Elements/Inventory/inventory.gd
+++ Godot 4.3/Elements/Inventory/inventory.gd
@@ -1,8 +1,18 @@
 class_name Inventory
 
+const EMPTY_ITEM = preload("res://Elements/Items/empty_item.tres")
 var _content:Array[ItemStackInstance] = []
+var slot_count:int = 14
 
-func add_item(new_stack: ItemStackInstance):
+func _init() -> void:
+	#Create empty slots
+	for i in slot_count:
+		var isi = ItemStackInstance.new()
+		isi.item = EMPTY_ITEM
+		isi.stack_count = 1
+		_content.append(isi)
+
+func add_item(new_stack: ItemStackInstance) -> bool:
 	#Loop through inventory checking for less than full stacks to fill
 	for inv_item_stack in get_contents():
 		#If new item is already in inventory, check if the stack is less than full
@@ -24,13 +34,21 @@ func add_item(new_stack: ItemStackInstance):
 		
 		#Exit function and loop if stack reaches zero
 		if new_stack.stack_count == 0:
-			return
+			return true
 	
+	#Loop through inventory checking for empty slots to fill
+	for i in _content.size():
+		if _content[i].item.name == EMPTY_ITEM.name:
+			_content[i] = new_stack
+			print("empty stacks")
+			return true
 	
-	#If loop finished with remaining count, create a new stack
-	_content.append(new_stack)
+	#Inventory is full, return false
+	print ("false")
+	return false
 
 func remove_item(item_stack: ItemStackInstance):
+	#empty the slot
 	_content.erase(item_stack)
 
 func get_contents() -> Array[ItemStackInstance]:
